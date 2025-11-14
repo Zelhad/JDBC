ac67adf9d9813bcb3659cc8145c7af9197bb5d5e
Zelhad <ze.elhadraoui@gmail.com>
2025-11-14 16:02:41 +0100

    Update productService List<BillingAccountRef> added

--- src/main/java/service/ProductService.java
+++ src/main/java/service/ProductService.java
@@ -7,6 +7,8 @@ import entities.Product;
 public interface ProductService {
 	List<Product> findAll() throws java.sql.SQLException;
 	List<AgreementItemRef> findAllAgreementItemRef () throws java.sql.SQLException;
+	List<entities.BillingAccountRef> findBillingAccountRef () throws java.sql.SQLException;
+	
 	
 
 }
